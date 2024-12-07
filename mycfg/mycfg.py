e987e131f057f518080de9f88412522f367549d2
gdymind <gdymind@gmail.com>
2024-12-06 22:55:46 -0600

    add cfg mapping

--- mycfg/mycfg.py
+++ mycfg/mycfg.py
@@ -16,17 +16,50 @@ def form_blocks(instrs):
                 yield current_block
                 current_block = []
         else: # label 
-            yield current_block
+            # if the current block is not empty, yield it
+            if current_block:
+                yield current_block
             current_block = [instr]
 
-    yield current_block
+    if current_block:
+        yield current_block
+
+def label_blocks(blocks):
+    mapping = {}
+    i = 0
+    for block in blocks:
+        if 'label' in block[0]:
+            name = block[0]['label']
+            block.pop(0)
+        else:
+            name = f"BLK_{i}"
+            i += 1
+        mapping[name] = block
+    return mapping
+
+def add_edges(name2block):
+    for _, block in name2block.items():
+        last_instr = block[-1]
+        if last_instr['op'] in TERMINATORS:
+            print(last_instr)
 
 def mycfg():
     program =  json.load(sys.stdin)
     for function in program['functions']:
-        for block in form_blocks(function['instrs']):
+        blocks = list(form_blocks(function['instrs']))
+        for block in blocks:
             print(block)
- 
+        name2block = label_blocks(blocks)
+        print(name2block)
+        # for name, block in name2block.items():
+        #     print(name)
+        #     for instr in block:
+        #         print(instr)
+        #     last_instr = block[-1]
+        #     # if the last instruction is a jump instruction,
+        #     # we need to add an edge
+        #     if last_instr['op'] == 'jmp':
+        #         print(last_instr)
 if __name__ == "__main__":
     mycfg()
  
